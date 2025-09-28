4411eb21d25b5226a180b3fd14f76ad81616e040
Bui Viet Trung <trung.bv@asahinainfotech.com>
2025-09-28 15:32:48 +0700

    Zoom in out themo hygrometer

--- src/objects/ThermoHygrometer.ts
+++ src/objects/ThermoHygrometer.ts
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import TextureKeys from 'consts/TextureKeys';
 
 export default class ThermoHygrometer extends Phaser.GameObjects.Image {
+  private isPointerOver: boolean = false;
 
   constructor(
     scene: Phaser.Scene,
@@ -20,5 +21,25 @@ export default class ThermoHygrometer extends Phaser.GameObjects.Image {
     scene.add.existing(this);
     scene.input.setDraggable(this);
     this.setVisible(false);
+
+    // Bắt sự kiện chuột vào/ra
+    this.on('pointerover', () => {
+      this.isPointerOver = true;
+    });
+
+    this.on('pointerout', () => {
+      this.isPointerOver = false;
+    });
+
+    // Lắng nghe lăn chuột
+    scene.input.on('wheel', (_pointer: any, _gameObjects: any, _dx: number, dy: number) => {
+      if (!this.isPointerOver) return; // chỉ zoom khi trỏ lên đối tượng này
+
+      if (dy > 0) {
+        this.setScale(this.scale * 0.9); // zoom out
+      } else {
+        this.setScale(this.scale * 1.1); // zoom in
+      }
+    });
   }
 }
